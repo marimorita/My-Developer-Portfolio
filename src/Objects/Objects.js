70bd36a81555d44f8ed8bffa4721318f660c6038
marimorita <marimm0927@gmail.com>
2025-03-17 18:30:56 -0500

    feat(experience, components): Add new JavaScript file to use an object. Use .map in experience and improve code

---
+++ src/Objects/Objects.js
@@ -0,0 +1,7 @@
+export const experienceObj = [{
+    role: "Full Stack Developer",
+    place: "EDUA (Empresa de desarrollo urbano de Armenia) Armenia's City Hall",
+    date: "2024-2025",
+    text: "This is my first job as a software developer. I developed an application for document management in the technical area of EDUA, at the Mayor’s Office of Armenia, Colombia. I worked independently since the company did not have a development team, which made it a great challenge and a valuable experience for my first job"
+}]
+
--- src/Pages/Experience/Experience.jsx
+++ src/Pages/Experience/Experience.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { GiBriefcase } from "react-icons/gi";
+import { experienceObj } from "../../Objects/Objects"
 
-export const Experience = ({refExperience}) => {
+export const Experience = ({ refExperience }) => {
   return (
-    <section ref={refExperience}>
+    (experienceObj.map((objectData)=> (<section ref={refExperience}>
       <div className='grid grid-cols-2 mt-26'>
         <div className='col-span-1'>
           <div className='flex flex-wrap '>
@@ -14,17 +15,17 @@ export const Experience = ({refExperience}) => {
             <div className='h-min-screen w-min-screen border-l-2 border-[#F2B5B9] relative'>
               <div className='h-[20px] w-[20px] absolute bg-[#DC676F] rounded-[100%] left-[-11px] top-2'></div>
               <div className='ml-4 mr-6'>
-                <h2 className='text-[#DC676F] text-[25px]'>Full Stack Developer</h2>
-                <h3 className='text-[#FF949B] text-[20px]'>EDUA (Empresa de desarrollo urbano de Armenia) Armenia's City Hall</h3>
-                <p className='text-[#776969] text-[18px]'> 2024-2025</p>
+                <h2 className='text-[#DC676F] text-[25px]'>{objectData.role}</h2>
+                <h3 className='text-[#FF949B] text-[20px]'>{objectData.place}</h3>
+                <p className='text-[#776969] text-[18px]'>{objectData.date}</p>
               </div>
             </div>
           </div>
         </div>
         <div className=' mt-18'>
-          <p className='text-[#DC676F] text-[20px] text-justify'>This is my first job as a software developer. I developed an application for document management in the technical area of EDUA, at the Mayor’s Office of Armenia, Colombia. I worked independently since the company did not have a development team, which made it a great challenge and a valuable experience for my first job</p>
+          <p className='text-[#DC676F] text-[20px] text-justify'>{objectData.text}</p>
         </div>
       </div>
-    </section>
+    </section>)) )
   )
 }
